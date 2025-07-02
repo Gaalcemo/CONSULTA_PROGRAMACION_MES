b93ba33800b2d595b309083c01014dc8d6db9396
Gabriel Alexander Celis Morales <168120072+Gaalcemo@users.noreply.github.com>
2025-07-02 17:53:07 +0200

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -8,10 +8,8 @@ export default async function handler(req, res) {
 
   try {
     const scriptUrl = `https://script.google.com/macros/s/AKfycbz97OzNUEULG_waTarAlESaR6nUXbxxz99L3bIr2nJFf7dpl2mdEmjs4XT6omdr_qj4eA/exec?dni=${encodeURIComponent(dni)}`;
-
     const response = await fetch(scriptUrl);
     const data = await response.json();
-
     res.status(200).json(data);
   } catch (error) {
     console.error("Error al consultar el script:", error);
